7a81609c3c19d4aebdb28ddc3c469f7c2ab1d80e
Simone <Simone@Simone-PC>
2016-08-18 22:16:44 -0300

    Configurando paginação na tabela de clientes.

--- application/controllers/cadastrocliente_control.php
+++ application/controllers/cadastrocliente_control.php
@@ -22,73 +22,38 @@ class cadastrocliente_control extends CI_Controller {
         $config = array(
             "base_url" => base_url('index.php/cadastrocliente/formulario'),
             "per_page" => 3,
-            "num_links" =>3,
+            "num_links" => 3,
             "uri_segment" => 3,
             "total_rows" => $this->cadastrocliente_model->countAll(),
-            "full_tag_open" => "<ul class='pagination'>",//tag de abertura
-            "full_tag_close" => "</ul>",//tag de fechamento
-            "first_link" => false,//link puxando para a primeira pagina
-            "last_link" => false,//link puxando para a ultima pagina
-            "first_tag_open" => "<li>",//tag de abertura do primeiro link
-            "first_tag_close" => "</li>",//tag de fechamento do primeiro link
-            "prev_link" => "Anterior",//conteudo a ser exibido para o link de paginação que leva a pagina anterior 
-            "prev_tag_open" => "<li class='prev'>",//tag de abertura para prev link
-            "prev_tag_close" => "</li>",//tag de fechamento para prev link
-            "next_link" => "Próxima",//conteudo a ser exibido para o link de paginação que leva a proxima pagina
-            "next_tag_open" => "<li class='next'>",//tag de abertura para next link
-            "next_tag_close" => "</li>",//tag de fechamento para next link
-            "last_tag_open" => "<li>",//tag de abertura para a ultima pagina
-            "last_tag_close" => "</li>",//tag de fechemento para a ultima pagina 
-            "cur_tag_open" => "<li class='active'><a href='#'>",//tag de abertura a ser adicionada para o item ativo
-            "cur_tag_close" => "</a></li>",//tag de fechamento a ser adicionada para o item ativo
-            "num_tag_open" => "<li>",//tag de abertura a ser adicionada para os itens numericos(as paginas da nossa aplicação
+            "full_tag_open" => "<ul class='pagination'>", //tag de abertura
+            "full_tag_close" => "</ul>", //tag de fechamento
+            "first_link" => false, //link puxando para a primeira pagina
+            "last_link" => false, //link puxando para a ultima pagina
+            "first_tag_open" => "<li>", //tag de abertura do primeiro link
+            "first_tag_close" => "</li>", //tag de fechamento do primeiro link
+            "prev_link" => "Anterior", //conteudo a ser exibido para o link de paginação que leva a pagina anterior 
+            "prev_tag_open" => "<li class='prev'>", //tag de abertura para prev link
+            "prev_tag_close" => "</li>", //tag de fechamento para prev link
+            "next_link" => "Próxima", //conteudo a ser exibido para o link de paginação que leva a proxima pagina
+            "next_tag_open" => "<li class='next'>", //tag de abertura para next link
+            "next_tag_close" => "</li>", //tag de fechamento para next link
+            "last_tag_open" => "<li>", //tag de abertura para a ultima pagina
+            "last_tag_close" => "</li>", //tag de fechemento para a ultima pagina 
+            "cur_tag_open" => "<li class='active'><a href='#'>", //tag de abertura a ser adicionada para o item ativo
+            "cur_tag_close" => "</a></li>", //tag de fechamento a ser adicionada para o item ativo
+            "num_tag_open" => "<li>", //tag de abertura a ser adicionada para os itens numericos(as paginas da nossa aplicação
             "num_tag_close" => "</li>"//tag de fechamento a ser adicionada para os itens numericos(as paginas da nossa aplicação
         );
-        
+
         $this->pagination->initialize($config);
-        
-        $data['paginacao'] = $this->pagination->create_links(); 
-        
+
+        $data['paginacao'] = $this->pagination->create_links();
+
         $offset = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
-        
+
         $data['clientesRetorno'] = $this->cadastrocliente_model->listaClientes('id', 'asc', $config['per_page'], $offset);
-        
+
         $this->load->view('cadastroCliente.php', $data);
-
-//        $total_geral = 3;
-//        $por_pagina = 2;
-//        $apartirde = $this->uri->segment(3);
-//        
-//
-//        $config['base_url'] = base_url('index.php/cadastrocliente/formulario');
-//        $config['total_rows'] = $total_geral;
-//        $config['per_page'] = $por_pagina;
-//
-//        $this->pagination->initialize($config);
-//
-//        $retornaClientes = $this->cadastrocliente_model->listaClientes(0, $apartirde, $por_pagina);
-//
-//        $dados = array(
-//            "clientesRetorno" => $retornaClientes,
-//            "paginacao" => $this->pagination->create_links()
-//        );
-//
-//        $this->load->view('cadastroCliente.php', $dados);
-    }
-
-    public function editaCliente() {
-        $id = $this->input->post("id");
-        $retornaCliente = $this->cadastrocliente_model->buscaPorId($id);
-
-        $cliente = array(
-            "id" => $retornaCliente->row()->id,
-            "nome" => $retornaCliente->row()->nome,
-            "email" => $retornaCliente->row()->email,
-            "cpf" => $retornaCliente->row()->cpf
-        );
-
-
-        echo json_encode($cliente);
     }
 
     public function salvarCliente() {
@@ -116,6 +81,21 @@ class cadastrocliente_control extends CI_Controller {
         }
 
         echo $retorno;
+    }
+
+    public function editaCliente() {
+        $id = $this->input->post("id");
+        $retornaCliente = $this->cadastrocliente_model->buscaPorId($id);
+
+        $cliente = array(
+            "id" => $retornaCliente->row()->id,
+            "nome" => $retornaCliente->row()->nome,
+            "email" => $retornaCliente->row()->email,
+            "cpf" => $retornaCliente->row()->cpf
+        );
+
+
+        echo json_encode($cliente);
     }
 
     public function removerCliente() {
--- application/models/cadastrocliente_model.php
+++ application/models/cadastrocliente_model.php
@@ -3,6 +3,8 @@ if(!defined('BASEPATH')) exit ('No direct script access allowed');
 
 class Cadastrocliente_model extends CI_Model {
     
+    var $tabela_clientes = "clientes";
+    
     function __construct() {
         parent::__construct();
     }
@@ -16,7 +18,7 @@ class Cadastrocliente_model extends CI_Model {
             $this->db->limit($limit, $offset);
         }
         
-        $result = $this->db->get("clientes");
+        $result = $this->db->get($this->tabela_clientes);
 
         if ($result->num_rows() > 0) {
             return $result -> result_array();
@@ -26,28 +28,28 @@ class Cadastrocliente_model extends CI_Model {
     }
 
     public function salvar($clientes) {
-        return $this->db->insert("Clientes", $clientes);
+        return $this->db->insert($this->tabela_clientes, $clientes);
     }
 
     public function editar($cliente) {
 
         $this->db->where('id', $cliente["id"]);
-        return $this->db->update("clientes", $cliente);
+        return $this->db->update($this->tabela_clientes, $cliente);
     }
 
     public function deletar($cliente) {
 
         $this->db->where('id', $cliente["id"]);
-        return $this->db->delete("clientes", $cliente);
+        return $this->db->delete($this->tabela_clientes, $cliente);
     }
 
     public function buscaPorId($id_cliente) {
         $this->db->where("id", $id_cliente);
-        return $this->db->get('clientes');
+        return $this->db->get($this->tabela_clientes);
     }
 
     function countAll() {
-        return $this->db->count_all("clientes");
+        return $this->db->count_all($this->tabela_clientes);
     }
 
 }
